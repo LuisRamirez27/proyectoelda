fcc689faddf981efd99924f85ffe40029aaf44df
Francisco Ramos <18030325@itcelaya.edu.mx>
2020-05-05 15:10:47 -0500

    Termine la Interfaz principal e hize la funcion del botton exit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,63 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    Button BtnCaptura,BtnRegistros,BtnExit;
+    @FXML
+    ImageView Iv_Dolar,Iv_Tabla,Iv_Salir;
+
+    Main main = new Main();
+    Alert alert = new Alert(Alert.AlertType.NONE);
+
+    Image image1 = new Image(getClass().getResourceAsStream("/images/dolar.png"));
+    Image image2 = new Image(getClass().getResourceAsStream("/images/ver.jpg"));
+    Image image3 = new Image(getClass().getResourceAsStream("/images/salir.jpg"));
+
+    EventHandler<ActionEvent> handler = new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+            if(event.getSource() == BtnCaptura){
+
+            }else if(event.getSource() == BtnRegistros){
+
+            }else if(event.getSource() == BtnExit){
+                Close();
+            }
+        }
+    };
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        BtnCaptura.setOnAction(handler);
+        BtnRegistros.setOnAction(handler);
+        BtnExit.setOnAction(handler);
+        Iv_Dolar.setImage(image1);
+        Iv_Tabla.setImage(image2);
+        Iv_Salir.setImage(image3);
+    }
+
+    private void Close(){
+        alert.setAlertType(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("CONFIRMACION");
+        alert.setContentText("¿Deseas salir del programa?");
+        alert.showAndWait();
+        if(alert.getResult() == ButtonType.CANCEL){
+            alert.close();
+        }else if(alert.getResult() == ButtonType.OK){
+            System.exit(0);
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,19 +3,44 @@ package sample;
 import interfaces.CapturarIngresos;
 import interfaces.interfazPrincipalEjemplo;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+    public static  Stage primaryStage;
+    Stage stage = new Stage();
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-       // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-       // primaryStage.setTitle("Hello World");
-        //primaryStage.setScene(new Scene(root, 300, 275));
-        //primaryStage.show();
-        //JOptionPane.showMessageDialog(null,"Hola Mundo");
+    public void start(Stage _primaryStage) throws Exception{
+        primaryStage = _primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Captura de Ingresos");
+        primaryStage.setScene(new Scene(root,400,275));
+        primaryStage.show();
         //new interfazPrincipalEjemplo();
-        new CapturarIngresos();
+        //new CapturarIngresos();
+    }
+
+    public void cargar(FXMLLoader loader, Object controller, ActionEvent event, Boolean size, String titulo) {
+        try {
+            Parent root= null;
+            loader.setController(controller);
+            root=loader.load();
+            Scene scene=new Scene(root);
+            stage.setTitle(titulo);
+            stage.setScene(scene);
+            stage.setMaximized(size);
+            stage.show();
+            ((Stage)(((Button) event.getSource()).getScene().getWindow())).hide();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -23,7 +48,4 @@ public class Main extends Application {
         launch(args);
     }
 
-    void cambio(){
-        //Notaaaaaaa
-    }
 }
